90d4502881aae3d895486414ea59b1cff52d0614
olivier <oliviermalik@yahoo.fr>
2019-04-03 23:02:12 +0200

    end tp security

--- src/test/java/com/miage/altea/tp/trainer_api/controller/TrainerControllerIntegrationTest.java
+++ src/test/java/com/miage/altea/tp/trainer_api/controller/TrainerControllerIntegrationTest.java
@@ -4,6 +4,7 @@ import com.miage.altea.tp.trainer_api.bo.Trainer;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
@@ -24,6 +25,20 @@ public class TrainerControllerIntegrationTest {
     @Autowired
     private TrainerController controller;
 
+    @Value("user")
+    private String username;
+
+    @Value("1cb284b5-7805-4483-9f90-7d52cce28dca")
+    private String password;
+
+    @Test
+    public void getTrainers_shouldThrowAnUnauthorized(){
+        var responseEntity = this.restTemplate
+                .getForEntity("http://localhost:" + port + "/trainers/Ash", Trainer.class);
+        assertNotNull(responseEntity);
+        assertEquals(401, responseEntity.getStatusCodeValue());
+    }
+
     @Test
     public void trainerController_shouldBeInstanciated(){
         assertNotNull(controller);
@@ -31,7 +46,9 @@ public class TrainerControllerIntegrationTest {
 
     @Test
     public void getTrainer_withNameAsh_shouldReturnAsh() {
-        var ash = this.restTemplate.getForObject("http://localhost:" + port + "/trainers/Ash", Trainer.class);
+        var ash = this.restTemplate
+                .withBasicAuth(username, password)
+                .getForObject("http://localhost:" + port + "/trainers/Ash", Trainer.class);
         assertNotNull(ash);
         assertEquals("Ash", ash.getName());
         assertEquals(1, ash.getTeam().size());
@@ -42,7 +59,9 @@ public class TrainerControllerIntegrationTest {
 
     @Test
     public void getAllTrainers_shouldReturnAshAndMisty() {
-        var trainers = this.restTemplate.getForObject("http://localhost:" + port + "/trainers/", Trainer[].class);
+        var trainers = this.restTemplate
+                .withBasicAuth(username, password)
+                .getForObject("http://localhost:" + port + "/trainers/", Trainer[].class);
         assertNotNull(trainers);
         assertEquals(2, trainers.length);
 
